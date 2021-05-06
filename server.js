905ee993aa9eed1757a5fe62f1de5bfa3d4c5445
Ismet Perenda <iperenda1@etf.unsa.ba>
2021-05-06 16:22:44 +0200

    izmjena

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 
 const express = require('express');
-const path = require('path');
 
+const path = require('path');
 const app = express();
 
-
-app.use(express.static('./dist/mop-angular-app'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mop-angular-app/'}),
-);
-
-
+app.use(express.static(__dirname + '/dist/mop-angular-app'));
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname+'/dist/mop-angular-app/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
